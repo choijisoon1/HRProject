919865f8763a99069190c892a702e4b477796036
choijisoon1 <choijisoon1@gmail.com>
2025-12-24 14:45:21 +0900

    input 컴포넌트 수정작업, 컴포넌트 가이드에 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,19 +1,39 @@
+// src/components/common/Input.tsx
 import React from 'react';
 import styles from './Input.module.scss';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    label: string;
+    /* 라벨 혹은 아이콘 */
+    label?: string;
     error?: string;
     required?: boolean;
+    icon?: React.ReactNode;
 }
 
-const Input = ({ label, error, required, className = '', ...props }: InputProps) => {
+const Input = ({ label, error, required, icon, className = '', ...props }: InputProps) => {
     return (
         <div className={`${styles.container} ${className}`}>
-            <label className={styles.label}>
-                {label} {required && <span className={styles.required}>*</span>}
-            </label>
-            <input className={`${styles.input} ${error ? styles.isError : ''}`} {...props} />
+            {/* 라벨이 있을 때만 */}
+            {label && (
+                <label className={styles.label}>
+                    {label} {required && <span className={styles.required}>*</span>}
+                </label>
+            )}
+
+            <div className={styles.inputWrapper}>
+                {/* 아이콘이 있을 때 */}
+                {icon && <span className={styles.icon}>{icon}</span>}
+
+                <input
+                    className={`
+                        ${styles.input} 
+                        ${error ? styles.isError : ''} 
+                        ${icon ? styles.hasIcon : ''}
+                    `}
+                    {...props}
+                />
+            </div>
+
             {error && <p className={styles.errorText}>{error}</p>}
         </div>
     );
