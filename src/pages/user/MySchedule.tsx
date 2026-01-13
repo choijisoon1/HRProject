6975de5339fd0917b154afd3fdb116307832eff4
choijisoon1 <choijisoon1@gmail.com>
2026-01-13 13:21:37 +0900

    머지

--- src/pages/user/MySchedule.tsx
+++ src/pages/user/MySchedule.tsx
@@ -80,7 +80,7 @@ const MySchedule = () => {
         <PageLayout>
             <PageHeader title="나의 일정" description="개인적인 업무와 할 일을 관리합니다." />
                 {/* 
-                  ratio="1fr 150px 100px" -> 내용(가변) : 날짜(고정) : 버튼(고정)
+                    ratio="1fr 150px 100px" -> 내용(가변) : 날짜(고정) : 버튼(고정)
                 */}
                 <Form onSubmit={handleAdd}>
                     <FormRow ratio="1fr 150px 100px">
@@ -99,7 +99,8 @@ const MySchedule = () => {
                         
                     </FormRow>
                 </Form>
-            
+                
+
                 {/* 리스트 영역 */}
                 <ul className={styles.todoList}>
                     {todos.map(todo => (
